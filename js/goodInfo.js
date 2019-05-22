7fd3f03d742b30aeb34a53796fe7c7db571e9af7
Curtainf2f <835074923@qq.com>
2019-05-23 01:47:30 +0800

    添加信息滚动

---
+++ js/goodInfo.js
@@ -0,0 +1,18 @@
+var goodInfoTimer;
+
+playGdb();
+
+function gdb(time, event, goods){
+    $("<dd style='height:0px;'><div class='gi-left'><span>"+time+"</span></div><div class='gi-right'><h4>"+event+"</h4><a href='#'>"+goods+"</a></div></dd>").prependTo('#idGoodsInfo dl');
+    var _first=$('#idGoodsInfo dl dd:first');
+	_first.animate({height: '+62px'}, "slow");
+	var _last=$('#idGoodsInfo dl dd:last');
+	_last.remove();
+}; 
+
+function playGdb() {
+    clearInterval(goodInfoTimer);
+    goodInfoTimer = setInterval(function () {
+        gdb("1小时前", "王**购买了", "商品x");
+    }, 3000);
+}
