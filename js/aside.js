71d576d2852366dde486362c9d7fb45397d1f410
Curtainf2f <835074923@qq.com>
2019-05-24 21:20:19 +0800

    添加侧边栏

---
+++ js/aside.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("aside ul li").hover(function(){
+		$(this).find(".sidebox").stop().animate({"width":"124px"},200).css({"opacity":"1","filter":"Alpha(opacity=100)","background":"#ae1c1c"})	
+	},function(){
+		$(this).find(".sidebox").stop().animate({"width":"54px"},200).css({"opacity":"0.8","filter":"Alpha(opacity=80)","background":"#000"})	
+	});
+	
+});
+
+function goTop(){
+	$('html,body').animate({'scrollTop':0},600);
+}
