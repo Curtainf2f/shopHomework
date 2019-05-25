f0b2d5418b944cc018d26f797ac9f05a62e44e7b
Curtainf2f <835074923@qq.com>
2019-05-25 20:07:50 +0800

    侧边栏搜索

--- js/aside.js
+++ js/aside.js
@@ -1,13 +1,46 @@
-$(document).ready(function(){
-
-	$("aside ul li").hover(function(){
-		$(this).find(".sidebox").stop().animate({"width":"124px"},200).css({"opacity":"1","filter":"Alpha(opacity=100)","background":"#ae1c1c"})	
-	},function(){
-		$(this).find(".sidebox").stop().animate({"width":"54px"},200).css({"opacity":"0.8","filter":"Alpha(opacity=80)","background":"#000"})	
-	});
-	
+$(document).ready(function() {
+    $("aside ul li #searchBox").hover(
+        function() {
+            $(this)
+                .animate({ width: "555px" }, 100)
+                .css({
+                    opacity: "1",
+                    filter: "Alpha(opacity=100)",
+                    background: "#ae1c1c"
+                });
+        },
+        function() {
+            $(this)
+                .animate({ width: "54px" }, 100)
+                .css({
+                    opacity: "0.8",
+                    filter: "Alpha(opacity=80)",
+                    background: "#000"
+                });
+        }
+    );
+    $("aside ul li #sideNormal").hover(
+        function() {
+            $(this)
+                .animate({ width: "124px" }, 200)
+                .css({
+                    opacity: "1",
+                    filter: "Alpha(opacity=100)",
+                    background: "#ae1c1c"
+                });
+        },
+        function() {
+            $(this)
+                .animate({ width: "54px" }, 200)
+                .css({
+                    opacity: "0.8",
+                    filter: "Alpha(opacity=80)",
+                    background: "#000"
+                });
+        }
+    );
 });
 
-function goTop(){
-	$('html,body').animate({'scrollTop':0},600);
+function goTop() {
+    $("html,body").animate({ scrollTop: 0 }, 600);
 }
